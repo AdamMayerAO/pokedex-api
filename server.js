7e97b4a7a72640634863796a486d0eae3921ac05
Adam Mayer <r.adammayer@gmail.com>
2021-03-17 21:45:00 -0400

    Heroku ready

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const helmet = require('helmet')
 const POKEDEX = require('./pokedex.json')
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 
 app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    console.log('validate bearer token middleware')
     
     if (!authToken || authToken.split(' ')[1] !==  apiToken){
         return res.status(401).json({error: 'Unauthorized request'})
@@ -44,10 +44,18 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
     res.json(response)
 })
 
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
 
+const PORT = process.env.PORT || 8000
 
-const PORT = 8000
-
-app.listen(PORT, (cb) => {
+app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
